709775752bdf2fde491cd16ba417414d174f1711
Paulo Viteri <undevoted@proton.me>
2025-11-22 22:53:57 -0500

    chore: weekly updates

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -3,7 +3,7 @@ local km = vim.keymap
 -- Common shortcuts
 km.set({ 'n', 'i', 'v', 'o' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save this buffer' })
 km.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Exit Neovim' })
-km.set('n', 'q', '<cmd>q<cr>', { desc = 'Close current buffer' })
+km.set('n', 'qw', '<cmd>q<cr>', { desc = 'Close current buffer' })
 
 -- Lazy.nvim
 km.set('n', '<leader>l', '<cmd>Lazy<cr>', { desc = 'Open Lazy.nvim' })
